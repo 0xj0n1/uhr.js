c6e5e0cfc925a0d7693184acb88c99e3fa20c96b
0xj0n1 <jonathangriesser3@gmail.com>
2022-09-14 13:21:33 +0200

    Zeitzone weg

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 (function time () {
-  document.getElementById("time").innerHTML = new Date().toString("hh:mm:ss tt");
+  document.getElementById("time").innerHTML = new Date().toString("hh:mm:ss ");
   var timeout = setTimeout(time, 1000); // recalls the function after 1000 ms
 })();
